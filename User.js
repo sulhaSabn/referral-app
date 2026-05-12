8951307091b459c46f1ad9c402876effe389b849
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-12 22:22:41 +0430

    Update User.js

--- User.js
+++ User.js
@@ -1,52 +1,20 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
+    username:{type:String,unique:true},
+    email:{type:String,unique:true},
+    password:String,
 
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
+    balance:{type:Number,default:0},
+    depositApproved:{type:Boolean,default:false},
 
-    password:{
-        type:String,
-        required:true
-    },
+    referralCode:{type:String,unique:true},
+    invitedBy:{type:String,default:null},
+    invitedCount:{type:Number,default:0},
 
-    balance:{
-        type:Number,
-        default:0
-    },
+    isAdmin:{type:Boolean,default:false},
 
-    depositApproved:{
-        type:Boolean,
-        default:false
-    },
-
-    referralCode:{
-        type:String,
-        unique:true
-    },
-
-    invitedBy:{
-        type:String,
-        default:null
-    },
-
-    invitedCount:{
-        type:Number,
-        default:0
-    },
-
-    isAdmin:{
-        type:Boolean,
-        default:false
-    }
+    resetToken:{type:String,default:null}
 });
 
 module.exports = mongoose.model("User", userSchema);
