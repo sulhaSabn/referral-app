2766997788b147aa0c13a9ff8d7fff74707624d0
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-12 22:01:41 +0430

    Update User.js

--- User.js
+++ User.js
@@ -1,12 +1,52 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: { type:String, required:true, unique:true },
-  password: { type:String, required:true },
-  balance: { type:Number, default:2 }, // هدیه ثبت‌نام
-  projects: [{ id:Number, price:Number, profit:Number }],
-  referralCode: { type:String, unique:true },
-  referredBy: { type:String }
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    balance:{
+        type:Number,
+        default:0
+    },
+
+    depositApproved:{
+        type:Boolean,
+        default:false
+    },
+
+    referralCode:{
+        type:String,
+        unique:true
+    },
+
+    invitedBy:{
+        type:String,
+        default:null
+    },
+
+    invitedCount:{
+        type:Number,
+        default:0
+    },
+
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
