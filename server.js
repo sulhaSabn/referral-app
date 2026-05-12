6ac569e0d97aeeca1ab8df0c21301966dde57b70
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-12 18:25:52 +0430

    Update server.js

--- server.js
+++ server.js
@@ -7,17 +7,16 @@ const authRoutes = require("./auth");
 const projectRoutes = require("./project");
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-// اتصال به دیتابیس
-mongoose.connect("mongodb://localhost:27017/referralApp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(()=>console.log("MongoDB Connected"))
-  .catch(err=>console.log(err));
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
-app.listen(5000, ()=>console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
