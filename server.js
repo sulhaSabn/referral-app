308e9f27e8d5aa8bb56f17f29c255dd83cee6da9
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-12 22:25:32 +0430

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const authRoutes = require("./auth");
 const projectRoutes = require("./project");
@@ -13,6 +15,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+
+app.use(rateLimit({
+    windowMs:15*60*1000,
+    max:100
+}));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
@@ -23,8 +31,6 @@ app.use("/api/projects", projectRoutes);
 app.use("/api/withdraw", withdrawRoutes);
 app.use("/api/admin", adminRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, ()=>{
-    console.log(`Server running on ${PORT}`);
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Server running");
 });
