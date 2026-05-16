61446895b74b99ce51d2ed10a88a5ccf06116a10
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-16 06:04:48 +0430

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { TronWeb } = require("tronweb");
-
+const getAvailableSignals = require("./signal");
 const router = express.Router();
 
 const User = require("./User");
